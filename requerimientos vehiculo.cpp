d51bf1572f1edd86dbf017aa18b05ece06c07cfe
ThunderDragonTitan <109685434+ThunderDragonTitan@users.noreply.github.com>
2022-07-28 10:40:55 -0500

    Add files via upload

--- requerimientos vehiculo.cpp
+++ requerimientos vehiculo.cpp
@@ -1,8 +1,12 @@
 /*Arbol binario tomando como referente la placa del vehiculo pla ca como serial unico
 	-Usar Inorden
 	-consultar por placa y mostrar la info del vehiculo
-	
-
+	-Mostrar informacion que contiene el arbol
+	-Mostrar solo los vehiculos de pasageros o los de carga
+	-Cuantos vehiculos son de determinado color
+	-Los vehiculos que tiene igualo superior potencia a una potencia dada 
+	-Consultar marcas de los vehiculos dados en el arbol
+	-Eliminar del arbol un vehiculo dado por la placa
 */
 class vehiculo
 {
