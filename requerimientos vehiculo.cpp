d3c528c77dd5080fd57ec6872e2b7137e3105149
ThunderDragonTitan <109685434+ThunderDragonTitan@users.noreply.github.com>
2022-07-28 10:33:51 -0500

    Add files via upload

---
+++ requerimientos vehiculo.cpp
@@ -0,0 +1,16 @@
+/*Arbol binario tomando como referente la placa del vehiculo pla ca como serial unico
+	-Usar Inorden
+	-consultar por placa y mostrar la info del vehiculo
+	
+
+*/
+class vehiculo
+{
+	private:
+		char *placa,*marca,*color;
+		int potencia; //caballos 
+		bool tipo;// si es de pasageros o es de carga
+		vehiculo *izq;
+		vehiculo *der;
+	public:
+}
